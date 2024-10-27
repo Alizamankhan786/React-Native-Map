5acf2e2fa1e2fbc034cf0d4a636a3ffbf38ef5c5
Alizamankhan786 <az030366@gmail.com>
2024-10-27 08:23:48 -0700

    location show nhi ho rhi

--- app/Mapsite.tsx
+++ app/Mapsite.tsx
@@ -1,13 +1,25 @@
 import { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet } from 'react-native';
+import { Platform, Text, View, StyleSheet, TextInput, TouchableOpacity, FlatList, ScrollView } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
-
-
 import * as Location from 'expo-location';
+
+
+interface SinglePlace {
+  latitude: number;
+  longitude: number
+}
+
+interface AllPlaces {
+  fsq_id: string;
+  name: string
+}
 
 const MapSite = () => {
   const [location, setLocation] = useState<null | any>(null);
   const [errorMsg, setErrorMsg] = useState<null | string>(null);
+  const [search, setSearch] = useState(``);
+  const [places , setPlaces] = useState <null | AllPlaces[]> (null);
+  const [singlesearchPlace , setsinglesearchPlace] = useState<null| SinglePlace>(null);
 
   useEffect(() => {
     (async () => {
@@ -25,6 +37,40 @@ const MapSite = () => {
     })();
   }, []);
 
+  const searchPlaces = () => {
+    if (!location || !search) {
+      console.error("Location or search term is not available");
+      return;
+    }
+  
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'fsq3qbL9ORBTq2ZaS6TUHxpAQZNDJjTlkT2lBeAynwmhZ8I='
+      }
+    };
+  
+    fetch(`fsq37ky2N/8LvcPO8+tc/4mi0SlpTsBSrFatKKKkFA6oxQ0=${search}&ll=${location.coords.latitude}%2C${location.coords.longitude}&radius=100000`, options)
+      .then(res => res.json())
+      .then(res => {
+        console.log(res.results);
+        setPlaces(res.results);
+      })
+      .catch(err => console.error(err));
+  };  
+
+  const singlePlaces = (item : any) => {
+    setPlaces(null);
+    setsinglesearchPlace({
+    latitude: item.geocodes.main.latitude,
+    longitude: item.geocodes.main.longitude,
+  });
+
+
+
+  }
+
   let text = 'Waiting..';
   if (errorMsg) {
     text = errorMsg;
@@ -34,9 +80,31 @@ const MapSite = () => {
 
 
   return (
-
     <View style={styles.container}>
     <Text style={styles.paragraph}>{text}</Text>
+    <TextInput
+  style={styles.input}
+  onChangeText={setSearch}
+  value={search}
+  placeholder="Search Locations"
+  placeholderTextColor="#888"
+/>
+
+    <TouchableOpacity onPress={searchPlaces} style={styles.button}>
+        <Text>Search</Text>
+      </TouchableOpacity>
+      {places && <FlatList
+         data={places}
+         renderItem={({item}: {item: {name: string}}) => {
+          return <View style={styles.list}>
+            <Text onPress={()=>singlePlaces(item)}>{item.name}</Text>
+          </View>
+         }}
+         keyExtractor={(item:{fsq_id: string}) => item.fsq_id
+         }
+        />}
+
+
     {location && <MapView style={styles.map} initialRegion={{
       latitude: location.coords.latitude,
       longitude: location.coords.longitude,
@@ -47,16 +115,19 @@ const MapSite = () => {
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
       }}>
+        {singlesearchPlace && <Marker coordinate={{
+          latitude: singlesearchPlace.latitude,
+          longitude: singlesearchPlace.longitude,
+        }}/>}
 
       </Marker>
     </MapView>
     
     
     }
-    </View>
-          
-  )
-}
+    </View>     
+  );
+};
 
 export default MapSite;
 
@@ -75,5 +146,28 @@ const styles = StyleSheet.create({
   map: {
     width: '100%',
     height: '70%',
+    marginTop: 40,
+  },
+  input: {
+    height: 40,
+    width: 180,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    marginTop: 20,
+    borderRadius: 40,
+    width: 110,
+  },
+  list: {
+    backgroundColor: 'gray',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    padding: 5,
+    width: 280
   }
 })
